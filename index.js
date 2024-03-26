6d55fff181783a66819da2d1db0761b675aef123
Sylvia <sylviachebet03@gmail.com>
2024-03-26 16:09:40 +0300

    add

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+// const titleCased = tutorials.map((tutorial) => tutorial.split (' '));
+// const capitalizeTutorials = titleCased.map(tutorial => tutorial.map((word) => word.charAt(0).toUpperCase() + word.slice(1)));
+// const joinCapTutorials= capitalizeTutorials.map((tutorial) => tutorial.join(" "));
+
+// function titleCased(tutorials) {
+//   const titleCasedTutorials = tutorials.map(tutorial => {
+//     return tutorial.split(' ').map(word => {
+//       return word.charAt(0).toUpperCase() + word.slice(1);
+//     }).join(' ');
+//   });
+  
+//   return titleCasedTutorials;
+// }
+
+function titleCased() {
+  // Use the global tutorials variable as data
+  return tutorials.map(tutorial => {
+    // Split each tutorial into words, capitalize the first letter of each word, and join them back into a single string
+    return tutorial.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
 }
